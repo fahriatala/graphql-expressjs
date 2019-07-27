4f4f9160c7c459528767116bd189c49cfc084243
fahri atala <fahriroviano@gmail.com>
2019-07-27 16:34:43 +0700

    add testing for middleware

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -2,16 +2,24 @@ const jwt = require('jsonwebtoken');
 const configJwt = require("../../config/config");
 
 module.exports = (req, res, next) => {
+    let decoded;
+    const authHeader = req.get('Authorization');
+    if (!authHeader) {
+        const error = new Error('Not authenticated.');
+        error.statusCode = 401;
+        throw error;
+    }
     try{
-        const token = req.headers.authorization.split(" ")[1];
+        // const token = req.headers.authorization.split(" ")[1];
+        const token = authHeader.split(' ')[1];
         console.log(token);
         // const decoded = jwt.verify(req.body.token, process.env.JWT_KEY);
-        const decoded = jwt.verify(token, configJwt.jwtKey);
+        decoded = jwt.verify(token, configJwt.jwtKey);
         req.userData = decoded;
-        next();
+        // next();
     } catch(error) {
-        return res.status(401).json({
-                message: 'Auth Failed'
-        })
+        throw error;
     }
-}
+    req.userId = decoded.userId;
+    next();
+};
